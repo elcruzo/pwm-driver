cfab046668f18f636d49fb080d4597b87d6c364e
Ayomide Adekoya <ayomideadekoya266@gmail.com>
2019-09-12 11:45:00 -0700

    add pwm interface

--- rtl/pwm_channel.v
+++ rtl/pwm_channel.v
@@ -1,7 +1,9 @@
 module pwm_channel (
-    input  wire clk,
-    input  wire rst_n,
-    output reg  pwm_out
+    input  wire       clk,
+    input  wire       rst_n,
+    input  wire       enable,
+    input  wire [7:0] duty,
+    output reg        pwm_out
 );
 
     assign pwm_out = 1'b0;
