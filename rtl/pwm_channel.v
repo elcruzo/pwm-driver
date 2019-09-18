2fef1e565a47b3a1b273a0ec019733e12ceb6b57
Ayomide Adekoya <ayomideadekoya266@gmail.com>
2019-09-18 10:30:00 -0700

    add duty cycle comparison

--- rtl/pwm_channel.v
+++ rtl/pwm_channel.v
@@ -15,4 +15,13 @@ module pwm_channel (
             counter <= counter + 1'b1;
     end
 
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            pwm_out <= 1'b0;
+        else if (enable)
+            pwm_out <= (counter < duty);
+        else
+            pwm_out <= 1'b0;
+    end
+
 endmodule
