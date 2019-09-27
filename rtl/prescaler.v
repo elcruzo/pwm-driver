43809e860ad15ae3c9df50c9a302df427b0824de
Ayomide Adekoya <ayomideadekoya266@gmail.com>
2019-09-27 11:30:00 -0700

    add prescaler module

---
+++ rtl/prescaler.v
@@ -0,0 +1,25 @@
+module prescaler (
+    input  wire        clk,
+    input  wire        rst_n,
+    input  wire [15:0] div,
+    output reg         tick
+);
+
+    reg [15:0] counter;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            counter <= 16'd0;
+            tick    <= 1'b0;
+        end else begin
+            if (counter >= div - 1) begin
+                counter <= 16'd0;
+                tick    <= 1'b1;
+            end else begin
+                counter <= counter + 1'b1;
+                tick    <= 1'b0;
+            end
+        end
+    end
+
+endmodule
